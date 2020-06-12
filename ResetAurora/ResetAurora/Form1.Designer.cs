56f068234f24ec1220c3c91b30fab9e2e1e2be8a
Vincent Wang <376787823@qq.com>
2020-06-12 12:18:13 +0800

    Upload reset program.

---
+++ ResetAurora/ResetAurora/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace ResetAurora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.LawnGreen;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.Location = new System.Drawing.Point(246, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.ForeColor = System.Drawing.Color.LawnGreen;
+            this.textBox1.Location = new System.Drawing.Point(3, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "请在此处输入原注册码";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Black;
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.checkBox1.Location = new System.Drawing.Point(186, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(60, 16);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "学习版";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(2, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 5;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ResetAurora.Properties.Resources._0833460;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(299, 187);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "重置Aurora [内部Build版]";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ResetAurora/ResetAurora/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Management;
+using System.Security.Cryptography;
+using System.Configuration;
+
+//加密密码
+//商业版加密密码ilovetangwei
+//学习版加密密码DateTime.Now.Date.ToString("yyyy-MM-dd") + "hupo"
+
+namespace ResetAurora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string strRC = "";       //rc
+        public string strEnctr = "";        //jia mi chuan
+        public string strKC = "";       //kc    
+        public string strInputKC = "";       //用于接收Form2的注册码,只有注册过的机器才可以重置.
+        public int nFlag = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = DialogResult.Yes;
+            dr = MessageBox.Show("确定要重置注册表?", "Aurora智能提示", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.No)
+            {
+                return;
+            }
+
+            strRC = CreateSNCode();
+            strInputKC = textBox1.Text;
+            
+            if (checkBox1.Checked == true)
+            {
+                strEnctr = EnText(strRC, DateTime.Now.Date.ToString("yyyy-MM-dd") + "hupo");       //学生版
+                if (transform(strEnctr, DateTime.Now.Date.ToString("yyyy-MM-dd") + "hupo") == strInputKC)
+                {
+                    //调用外部程序导cmd命令行,实际上没Diao用
+                    Process p = new Process();
+                    p.StartInfo.FileName = "cmd.exe";
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardInput = true;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.CreateNoWindow = false;
+                    p.Start();
+                    //向cmd.exe输入command 
+                    p.StandardInput.WriteLine("ipconfig");
+                    p.StandardInput.WriteLine("exit");     //需要有这句，不然程序会挂机
+
+                    RegistryKey MyReg, RegGUIDType, RegType, RegValidGUIDDays, RegValidDays, RegSuccess, RegCodeAllow, RegStartGUIDDate, RegStartDate;//声明注册表对象
+                    MyReg = Registry.CurrentUser;//获取当前用户注册表项
+                    try
+                    {
+                        RegGUIDType = MyReg.CreateSubKey("Identities\\{D46B7C02-B796-4AAA-9D4F-2188CF2DBA30}");//在注册表项中创建子项
+                        RegType = MyReg.CreateSubKey("Software\\Aurora");
+
+                        RegValidGUIDDays = MyReg.CreateSubKey("Identities\\{D46B7C02-B796-4AAA-9D4F-2188CF2DBA30}");
+                        RegValidDays = MyReg.CreateSubKey("Software\\Aurora");
+
+                        RegCodeAllow = MyReg.CreateSubKey("Identities\\{D46B7C02-B796-4AAA-9D4F-2188CF2DBA30}");
+                        RegSuccess = MyReg.CreateSubKey("Identities\\{D46B7C02-B796-4AAA-9D4F-2188CF2DBA30}");
+                        RegStartGUIDDate = MyReg.CreateSubKey("Identities\\{D46B7C02-B796-4AAA-9D4F-2188CF2DBA30}");
+                        RegStartDate = MyReg.CreateSubKey("Software\\Aurora");
+
+                        RegType.SetValue("nRegFlag", "0");
+                        RegValidDays.SetValue("nValidDays", "10");             //重置为初始
+                        RegGUIDType.SetValue("nRegGUIDFlag", "0");
+                        RegValidGUIDDays.SetValue("nValidGUIDDays", "10");
+                        RegCodeAllow.SetValue("RegCodeAllow", "1");
+                        RegSuccess.SetValue("RegSuccess", "false");
+                        RegStartGUIDDate.SetValue("StartGUIDDate", DateTime.Now.ToString("yyyy-MM-dd"));
+                        RegStartDate.SetValue("StartDate", DateTime.Now.ToString("yyyy-MM-dd"));
+
+                        label1.Visible = true;
+                        label1.Text = "注册表信息已成功恢复初始设置，重启Aurora生效。";
+                        label1.TextAlign = ContentAlignment.MiddleCenter;
+                        label1.ForeColor = Color.LawnGreen;
+                    }
+                    catch { }
+                }
+                else
+                {
+                    label1.Visible = true;
+                    label1.Text = "                        注册码输入错误。";
+                    label1.TextAlign = ContentAlignment.MiddleCenter;
+                    label1.ForeColor = Color.Red;
+                }
+            } 
+            else
+            {
+                strEnctr = EnText(strRC, "ilovetangwei");       //商业版
+                if (transform(strEnctr, "ilovetangwei") == strInputKC)
+                {
+                    //调用外部程序导cmd命令行，实际上没Diao用
+                    Process p = new Process();
+                    p.StartInfo.FileName = "cmd.exe";
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardInput = true;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.CreateNoWindow = false;
+                    p.Start();
+                    //向cmd.exe输入command 
+                    p.StandardInput.WriteLine("ipconfig");
+                    p.StandardInput.WriteLine("exit");     //需要有这句，不然程序会挂机
+
+                    RegistryKey MyReg, RegGUIDType, RegType, RegValidGUIDDays, RegValidDays, RegSuccess, RegCodeAllow, RegStartGUIDDate, RegStartDate;//声明注册表对象
+                    MyReg = Registry.CurrentUser;//获取当前用户注册表项
+                    try
+                    {
+                        RegGUIDType = MyReg.CreateSubKey("Identities\\{D46B7C02-B796-4AAA-9D4F-2188CF2DBA30}");//在注册表项中创建子项
+                        RegType = MyReg.CreateSubKey("Software\\Aurora");
+
+                        RegValidGUIDDays = MyReg.CreateSubKey("Identities\\{D46B7C02-B796-4AAA-9D4F-2188CF2DBA30}");
+                        RegValidDays = MyReg.CreateSubKey("Software\\Aurora");
+
+                        RegCodeAllow = MyReg.CreateSubKey("Identities\\{D46B7C02-B796-4AAA-9D4F-2188CF2DBA30}");
+                        RegSuccess = MyReg.CreateSubKey("Identities\\{D46B7C02-B796-4AAA-9D4F-2188CF2DBA30}");
+                        RegStartGUIDDate = MyReg.CreateSubKey("Identities\\{D46B7C02-B796-4AAA-9D4F-2188CF2DBA30}");
+                        RegStartDate = MyReg.CreateSubKey("Software\\Aurora");
+
+                        RegType.SetValue("nRegFlag", "0");
+                        RegValidDays.SetValue("nValidDays", "10");             //重置为初始
+                        RegGUIDType.SetValue("nRegGUIDFlag", "0");
+                        RegValidGUIDDays.SetValue("nValidGUIDDays", "10");
+                        RegCodeAllow.SetValue("RegCodeAllow", "1");
+                        RegSuccess.SetValue("RegSuccess", "false");
+                        RegStartGUIDDate.SetValue("StartGUIDDate", DateTime.Now.ToString("yyyy-MM-dd"));
+                        RegStartDate.SetValue("StartDate", DateTime.Now.ToString("yyyy-MM-dd"));
+
+                        label1.Visible = true;
+                        label1.Text = "注册表信息已成功恢复初始设置，重启Aurora生效。";
+                        label1.ForeColor = Color.LawnGreen;
+                    }
+                    catch { }
+                }
+                else
+                {
+                    label1.Visible = true;
+                    label1.Text = "                        注册码输入错误。";
+                    label1.ForeColor = Color.Red;
+                }
+            }
+
+        }
+
+        //获得CPU的序列号
+        public string getCpu()
+        {
+            string strCpu = null;
+            ManagementClass myCpu = new ManagementClass("win32_Processor");
+            ManagementObjectCollection myCpuConnection = myCpu.GetInstances();
+            foreach (ManagementObject myObject in myCpuConnection)
+            {
+                strCpu = myObject.Properties["Processorid"].Value.ToString();
+                break;
+            }
+            return strCpu;
+        }
+
+        //取得设备硬盘的卷标号
+        public string GetDiskVolumeSerialNumber()
+        {
+            ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObject disk = new ManagementObject("win32_logicaldisk.deviceid=\"c:\"");
+            disk.Get();
+            return disk.GetPropertyValue("VolumeSerialNumber").ToString();
+        }
+
+        //生成机器码
+        public string CreateSNCode()
+        {
+            string temp = getCpu() + GetDiskVolumeSerialNumber();//获得24位Cpu和硬盘序列号
+            string[] strid = new string[24];//
+            for (int i = 0; i < 24; i++)//把字符赋给数组
+            {
+                strid[i] = temp.Substring(i, 1);
+            }
+            temp = "";
+            //Random rdid = new Random();
+            for (int i = 0; i < 24; i++)//从数组随机抽取24个字符组成新的字符生成机器三
+            {
+                //temp += strid[rdid.Next(0, 24)];
+                temp += strid[i + 3 >= 24 ? 0 : i + 3];
+            }
+            return GetMd5(temp);
+        }
+
+        public string GetMd5(object text)
+        {
+            string path = text.ToString();
+
+            MD5CryptoServiceProvider MD5Pro = new MD5CryptoServiceProvider();
+            Byte[] buffer = Encoding.GetEncoding("utf-8").GetBytes(text.ToString());
+            Byte[] byteResult = MD5Pro.ComputeHash(buffer);
+
+            string md5result = BitConverter.ToString(byteResult).Replace("-", "");
+            return md5result;
+        }
+
+        //加密数据
+        private string EnText(string Text, string sKey)
+        {
+            StringBuilder ret = new StringBuilder();
+            try
+            {
+                DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+                byte[] inputByteArray;
+                inputByteArray = Encoding.Default.GetBytes(Text);
+                //通过两次哈希密码设置对称算法的初始化向量   
+                des.Key = ASCIIEncoding.ASCII.GetBytes(System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile
+                (System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(sKey, "md5").Substring(0, 8), "sha1").Substring(0, 8));
+                //通过两次哈希密码设置算法的机密密钥   
+                des.IV = ASCIIEncoding.ASCII.GetBytes(System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile
+                (System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(sKey, "md5").Substring(0, 8), "md5").Substring(0, 8));
+                System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write);
+                cs.Write(inputByteArray, 0, inputByteArray.Length);
+                cs.FlushFinalBlock();
+                foreach (byte b in ms.ToArray())
+                {
+                    ret.AppendFormat("{0:X2}", b);
+                }
+                return ret.ToString();
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        //解密数据
+        private string DeText(string Text, string sKey)
+        {
+            try
+            {
+                DESCryptoServiceProvider des = new DESCryptoServiceProvider();   //定义DES加密对象   
+                int len;
+                len = Text.Length / 2;
+                byte[] inputByteArray = new byte[len];
+                int x, i;
+                for (x = 0; x < len; x++)
+                {
+                    i = Convert.ToInt32(Text.Substring(x * 2, 2), 16);
+                    inputByteArray[x] = (byte)i;
+                }
+                //通过两次哈希密码设置对称算法的初始化向量   
+                des.Key = ASCIIEncoding.ASCII.GetBytes(System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile
+                (System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(sKey, "md5").Substring(0, 8), "sha1").Substring(0, 8));
+                //通过两次哈希密码设置算法的机密密钥   
+                des.IV = ASCIIEncoding.ASCII.GetBytes(System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile
+                (System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(sKey, "md5").Substring(0, 8), "md5").Substring(0, 8));
+                System.IO.MemoryStream ms = new System.IO.MemoryStream();//定义内存流
+                CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write);//定义加密流
+                cs.Write(inputByteArray, 0, inputByteArray.Length);
+                cs.FlushFinalBlock();
+                return Encoding.Default.GetString(ms.ToArray());
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        //将加密的字符串转换为注册码形式
+        public string transform(string input, string skey)
+        {
+            string transactSn = string.Empty;
+            if (input == "")
+            {
+                return transactSn;
+            }
+            string initSn = string.Empty;
+            try
+            {
+                initSn = this.EnText(this.EnText(input, skey), skey).ToString();
+                transactSn = initSn.Substring(0, 5) + "-" + initSn.Substring(5, 5) +
+                "-" + initSn.Substring(10, 5) + "-" + initSn.Substring(15, 5) +
+                "-" + initSn.Substring(20, 5);
+                return transactSn;
+            }
+            catch
+            {
+                return transactSn;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+        }
+
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "请在此处输入原注册码";
+            }
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "请在此处输入原注册码")
+            {
+                textBox1.Text = "";
+            }
+        }
+
+        #region 拖动图片移动窗体
+        //鼠标拖动相关变量
+        Point oldPoint = new Point(0, 0);
+        bool mouseDown = false;
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            oldPoint = e.Location;
+            mouseDown = true;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Left += (e.X - oldPoint.X);
+                this.Top += (e.Y - oldPoint.Y);
+            }
+        }
+        #endregion
+    }
+}
